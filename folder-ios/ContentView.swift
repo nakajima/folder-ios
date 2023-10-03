d064bcb2b408650b648a41fbc4e610f7c70355ee
Pat Nakajima <patnakajima@gmail.com>
2023-10-03 13:05:45 -0700

    Initial Commit

---
+++ folder-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  folder-ios
+//
+//  Created by Pat Nakajima on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ folder-ios/folder_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  folder_iosApp.swift
+//  folder-ios
+//
+//  Created by Pat Nakajima on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct folder_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
